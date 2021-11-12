256c003b653d3826e4569ec5821dc31f3162302a
2933909306@qq.com <2933909306@qq.com>
2021-11-12 11:40:17 +0800

    外部demo的使用

--- config/config.ts
+++ config/config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   mode: 'site',
+  resolve: {
+    passivePreview: true,
+  },
   navs: [
     {
       title: '指南',
---
+++ src/components/foo/demos/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Foo } from 'lycg';
+
+export default () => <Foo title="First Demo" />;
