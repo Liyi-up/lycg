1d1fbaaee3bcf447f5dae47a7c4f6d0a793c4cfd
2933909306@qq.com <2933909306@qq.com>
2022-03-22 10:52:35 +0800

    feat: 跟新组件库

--- src/components/back-to-top/index.tsx
+++ src/components/back-to-top/index.tsx
@@ -1,5 +1,5 @@
 import './back-to-top.less';
 import { BackToTop } from './back-to-top';
-export type { BackToTopProps } from './back-to-top';
+export  type { BackToTopProps } from './back-to-top';
 
 export default BackToTop;
